4a341908d0ea8de48581b3372851358c0acc4565
Junior Martinez <78454970+junior-debug@users.noreply.github.com>
2022-03-30 18:29:18 -0700

    miura

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script>
+import Modal from '@/components/Modal.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    Modal
+  },
+}
+</script>
+<template>
+  <main class="main">
+    <Modal />
+  </main>
+</template>
+<style scoped>
+  .main {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
